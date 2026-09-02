fc2a5f123752b83b445925150579be8cd7078807
ByjuIttyK <byjuitty@gmail.com>
2026-09-02 09:03:44 +0530

    Vchr lst left outer join

--- HayatDb.js
+++ HayatDb.js
@@ -10239,9 +10239,9 @@ app.get("/api/vchrlst/:tranId", function (req, res) {
   if (req.params.tranId !== '05') {
     connection.query(
       "SELECT TRAN_TYPE, VCHR_NO, DATE_FORMAT(DATTE,'%d/%m/%Y') DATTE, "+
-      "CASE WHEN TRAN_TYPE = '02' THEN CUST_CODE ELSE ACC_CODE END AS ACC_CODE,CUST_CODE,  CHEQUE_NO, AMOUNT, NARRATION1, NARRATION2, AC_HEAD AS ACC_HEAD," +
+      "CASE WHEN ACC_CODE IS NULL THEN CUST_CODE ELSE ACC_CODE END AS ACC_CODE,CUST_CODE,  CHEQUE_NO, AMOUNT, NARRATION1, NARRATION2, AC_HEAD AS ACC_HEAD," +
       " BANK_NAME, PAID_TO, CAN_CEL," +
-      " ACC_CODE2, AMOUNT2, JOB_NO,  CUR_CODE, CONV_RATE, AMOUNT_FRGN FROM vouchers  left outer join ac_list  on ac_code = acc_code   WHERE TRAN_TYPE=? order by VCHR_NO desc",
+      " ACC_CODE2, AMOUNT2, JOB_NO,  CUR_CODE, CONV_RATE, AMOUNT_FRGN FROM vouchers  left outer join ac_list  on ac_list.ac_code = vouchers.acc_code   WHERE TRAN_TYPE=? order by VCHR_NO desc",
       [req.params.tranId],
 
       function (error, result) {
